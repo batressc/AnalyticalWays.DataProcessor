ccd1e67d87f3bb6b1a87f6d3fa15cf2a6e2325aa
Luis Gustavo Fernandez Batres <elclon_fernanbrans@hotmail.com>
2020-11-10 16:54:21 -0600

    - Implementacion con ADO.NET - Implementacion con EF Core & Bulk Insert - Limpieza de proyecto

--- AnalyticalWays.Utilities/Model/StockInformation.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AnalyticalWays.Utilities.Model {
-    public class StockInformation {
-        public string PointOfSale { get; set; }
-        public string Product { get; set; }
-        public string Date { get; set; }
-        public int Stock { get; set; }
-    }
-}
